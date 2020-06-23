0a9a47cd052f9303ea2042c7f928c528548c8d25
Wei Su <suwei@fsl.cs.sunysb.edu>
2020-06-23 18:03:50 +0000

    Fix implementation of Directory class

--- src/directory.cpp
+++ src/directory.cpp
@@ -14,8 +14,24 @@
 
 #include "inode.hpp"
 #include "directory.hpp"
+#include "util.hpp"
 
 using namespace std;
+
+void Directory::UpdateSize(ssize_t delta) {
+    /* Avoid negative result */
+    if (delta < 0 && -delta > m_fuseEntryParam.attr.st_size)
+        return;
+
+    m_fuseEntryParam.attr.st_size += delta;
+    m_fuseEntryParam.attr.st_blocks = get_nblocks(m_fuseEntryParam.attr.st_size, Inode::BufBlockSize);
+}
+
+void Directory::Initialize(fuse_ino_t ino, mode_t mode, nlink_t nlink, gid_t gid, uid_t uid) {
+    Inode::Initialize(ino, mode, nlink, gid, uid);
+    size_t base_size = sizeof(m_children);
+    UpdateSize(base_size);
+}
 
 /**
  Returns a child inode number given the name of the child.
@@ -31,21 +47,63 @@ fuse_ino_t Directory::ChildInodeNumberWithName(const string &name) {
     return m_children[name];
 }
 
+/**
+ Add a child entry to this directory.
+
+ @param name The name of the child to add.
+ @param ino The corresponding inode number.
+ @return 0 if successful, or errno if error occurred
+ */
+int Directory::AddChild(const string &name, fuse_ino_t ino) {
+    if (m_children.find(name) != m_children.end())
+        return -EEXIST;
+
+    const auto [it, success] = m_children.insert({name, ino});
+    if (!success)
+        return -ENOMEM;
+
+    size_t elem_size = sizeof(_Rb_tree_node_base) + sizeof(fuse_ino_t) + sizeof(std::string) + name.size();
+    UpdateSize(elem_size);
+    return 0;
+}
 
 /**
  Changes the inode number of the child with the given name.
 
  @param name The name of the child to update.
  @param ino The new inode number.
- @return The old inode number before the change.
+ @return 0 if successful, or errno if error occurred
  */
-fuse_ino_t Directory::UpdateChild(const string &name, fuse_ino_t ino) {
-    fuse_ino_t ino_ret = m_children[name];
+int Directory::UpdateChild(const string &name, fuse_ino_t ino) {
+    if (m_children.find(name) == m_children.end())
+        return -ENOENT;
+
     m_children[name] = ino;
     
     // TODO: What about directory sizes? Shouldn't we increase the reported size of our dir?
+    // NOTE: This is an **update** function, why should we care about increasing size here?
     
-    return ino_ret;
+    return 0;
+}
+
+/**
+ Remove a child entry in this directory
+ 
+ WARNING: This method does NOT check if the child is a non-empty directory!
+          Make sure the caller check this before calling!
+
+ @param name The name of the child to delete.
+ @return 0 if successful, or errno if error occurred
+ */
+int Directory::RemoveChild(const string &name) {
+    auto child = m_children.find(name);
+    if (child == m_children.end())
+        return -ENOENT;
+
+    m_children.erase(child);
+    size_t elem_size = sizeof(_Rb_tree_node_base) + sizeof(fuse_ino_t) + sizeof(std::string) + name.size();
+    UpdateSize(-elem_size);
+    return 0;
 }
 
 int Directory::WriteAndReply(fuse_req_t req, const char *buf, size_t size, off_t off) {
--- src/directory.hpp
+++ src/directory.hpp
@@ -8,11 +8,16 @@
 class Directory : public Inode {
 private:
     std::map<std::string, fuse_ino_t> m_children;
+
+    void UpdateSize(ssize_t delta);
 public:
     ~Directory() {}
-    
+
+    void Initialize(fuse_ino_t ino, mode_t mode, nlink_t nlink, gid_t gid, uid_t uid);
     fuse_ino_t ChildInodeNumberWithName(const std::string &name);
-    fuse_ino_t UpdateChild(const std::string &name, fuse_ino_t ino);
+    int AddChild(const std::string &name, fuse_ino_t);
+    int UpdateChild(const std::string &name, fuse_ino_t ino);
+    int RemoveChild(const std::string &name);
     int WriteAndReply(fuse_req_t req, const char *buf, size_t size, off_t off);
     int ReadAndReply(fuse_req_t req, size_t size, off_t off);
     
