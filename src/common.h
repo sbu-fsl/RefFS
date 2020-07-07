e6d082ba2999105e2865b2956c71a071afaf247e
Wei Su <suwei@fsl.cs.sunysb.edu>
2020-07-07 17:39:03 +0000

    Add a common.h that includes frequently used headers

---
+++ src/common.h
@@ -0,0 +1,25 @@
+#ifndef _COMMON_INCLUDE_H
+#define _COMMON_INCLUDE_H
+
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <shared_mutex>
+#include <map>
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <cerrno>
+#include <cassert>
+#include <cstring>
+
+#ifdef __APPLE__
+#include <osxfuse/fuse/fuse_lowlevel.h>
+#else
+#include <fuse/fuse_lowlevel.h>
+#endif
+#include <unistd.h>
+
+#include "util.hpp"
+
+#endif
