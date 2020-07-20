dfaf628110869aa05e51dcfedec30f0fd792c734
Wei Su <suwei@fsl.cs.sunysb.edu>
2020-07-20 20:03:20 +0000

    Take size and inodes parameters from the cmd arg in main program

--- src/main.cpp
+++ src/main.cpp
@@ -33,27 +33,32 @@ void delete_args(int argc, char **argv) {
 int main(int argc, const char * argv[]) {
     char **fuse_argv = copy_args(argc, argv);
     
-    struct fuse_args args = FUSE_ARGS_INIT(argc, fuse_argv);
+    struct fuse_args args = {argc, fuse_argv, 1};
     struct fuse_chan *ch;
+    struct fuse_ramfs_options options = {0};
     char *mountpoint;
     int err = -1;
 
     std::srand(std::time(nullptr));
     
+    /* Parse command-line args by fuse-ramfs */
+    opterr = 0;
+    ramfs_parse_cmdline(args, options);
     // The core code for our filesystem.
-    FuseRamFs core;
+    size_t nblocks = options.capacity / Inode::BufBlockSize;
+    FuseRamFs core(nblocks, options.inodes);
     
-    int mt = 1;
-    if (fuse_parse_cmdline(&args, &mountpoint, &mt, NULL) != -1) {
+    if (options.subtype) {
+        mountpoint = options.mountpoint;
         if (mountpoint == NULL) {
             cerr << "USAGE: fuse-cpp-ramfs MOUNTPOINT" << endl;
         } else if ((ch = fuse_mount(mountpoint, &args)) != NULL) {
             struct fuse_session *se;
-            
             // The FUSE options come from our core code.
             se = fuse_lowlevel_new(&args, &(core.FuseOps),
                                    sizeof(core.FuseOps), NULL);
             if (se != NULL) {
+                fuse_daemonize(options.deamonize == 0);
                 if (fuse_set_signal_handlers(se) != -1) {
                     fuse_session_add_chan(se, ch);
                     err = fuse_session_loop(se);
