f60962a025cf55cb99b8649521e403a7a70b1fe0
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-12-19 16:30:44 -0500

    Add copyright header to each source code for RefFS

--- src/special_inode.hpp
+++ src/special_inode.hpp
@@ -1,5 +1,25 @@
-/** @file special_inode.hpp
- *  @copyright 2016 Peter Watkins. All rights reserved.
+/*
+ * This file is part of RefFS.
+ * 
+ * Copyright (c) 2020-2024 Yifei Liu
+ * Copyright (c) 2020-2024 Wei Su
+ * Copyright (c) 2020-2024 Erez Zadok
+ * Copyright (c) 2020-2024 Stony Brook University
+ * Copyright (c) 2020-2024 The Research Foundation of SUNY
+ * Original Copyright (C) Peter Watkins
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RefFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef special_inode_hpp
--- tests/compile.sh
+++ tests/compile.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+#
+# This file is part of RefFS.
+# 
+# Copyright (c) 2020-2024 Yifei Liu
+# Copyright (c) 2020-2024 Pei Liu
+# Copyright (c) 2020-2024 Wei Su
+# Copyright (c) 2020-2024 Erez Zadok
+# Copyright (c) 2020-2024 Stony Brook University
+# Copyright (c) 2020-2024 The Research Foundation of SUNY
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RefFS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
 xargs rm < ../build/install_manifest.txt
 rm -rf ../build/*
 cd ../build/ || exit
